9187410521e736f365b4deed37fb2ef2b8a72505
Idris <idris.maps@gmx.ch>
2016-05-24 16:37:25 +0200

    map

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 var enableCtrl = require('./lib/ctrl')
+var Map = require('./lib/objects/Map')
 
-enableCtrl()
+var mapView = new Map('map')
+enableCtrl(mapView)
--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function() {
+module.exports = function(map) {
 	var plans = document.getElementById('plans')
 	var saveDraw = document.getElementById('save-drawing')
 	var hideListBtn = document.getElementById('hide-side')
@@ -22,10 +22,12 @@ module.exports = function() {
 	}
 
 	drawBtn.onclick = function() {
+		map.startDraw()
 		plans.style.display = 'none'
 		drawCancelBtn.style.display = ''
 		drawSaveBtn.style.display = ''
 		drawCancelBtn.onclick = function() {
+			map.endDraw()
 			plans.style.display = ''
 			drawCancelBtn.style.display = 'none'
 			drawSaveBtn.style.display = 'none'	
@@ -40,7 +42,7 @@ module.exports = function() {
 			}
 			planNameBtn.onclick = function() {
 				if(planNameInput.value) {
-					console.log('saving ' + planNameInput.value)
+					console.log('saving', planNameInput.value, map.points)
 					planNameInput.value = ''
 					saveDraw.style.display = 'none'
 					plans.style.display = ''
@@ -51,8 +53,52 @@ module.exports = function() {
 }
 
 },{}],2:[function(require,module,exports){
+module.exports = function(mapId) {
+	var self = this
+	
+	var map = L.map(mapId).setView([46.517, 6.563], 17)
+	self.map = map
+
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map)
+
+	self.planLayer = new L.FeatureGroup().addTo(map)
+
+	self.drawing = false
+	self.points = []
+
+	self.map.on('click', function(e) {
+		if(self.drawing) {
+			self.points.push(e.latlng)
+			self.planLayer.clearLayers()
+			if(self.points.length === 1) {
+				var pt = L.marker(self.points[0])
+				self.planLayer.addLayer(pt)
+			} else {
+				var li = L.polyline(self.points)
+				self.planLayer.addLayer(li)
+			}
+		}
+	})
+
+	self.startDraw = function() {
+		self.planLayer.clearLayers()
+		self.drawing = true
+	}
+	self.endDraw = function() {
+		self.drawing = false
+		self.planLayer.clearLayers()
+		self.points = []
+	}
+}
+
+},{}],3:[function(require,module,exports){
 var enableCtrl = require('./lib/ctrl')
+var Map = require('./lib/objects/Map')
 
-enableCtrl()
+var mapView = new Map('map')
+enableCtrl(mapView)
 
-},{"./lib/ctrl":1}]},{},[2]);
+},{"./lib/ctrl":1,"./lib/objects/Map":2}]},{},[3]);
