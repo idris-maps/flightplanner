5152addcf12aa2cd8e10b8a20ccd91163bfec91b
Idris <idris.maps@gmx.ch>
2016-05-24 17:31:24 +0200

    Plan Plans Store objects

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 var enableCtrl = require('./lib/ctrl')
 var Map = require('./lib/objects/Map')
+var Plans = require('./lib/objects/Plans')
+var Store = require('./lib/objects/Store')
+
 window.onload = function() {
-var mapView = new Map('map')
-enableCtrl(mapView)
+	var mapView = new Map('map')
+	var store = new Store()
+	var plans = new Plans('plan-list', store, mapView)
+	enableCtrl(mapView, plans)
+
+	plans.init()
+
 }
